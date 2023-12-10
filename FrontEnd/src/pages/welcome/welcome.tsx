f2978d02812d4f7f0aeedd6c04ccedf25110fde9
Daniel Whyte <daniel.whyte@rymanhealthcare.com>
2023-12-11 11:52:55 +1300

    add animations to home page

--- FrontEnd/src/pages/welcome/welcome.tsx
+++ FrontEnd/src/pages/welcome/welcome.tsx
@@ -2,62 +2,97 @@ import React, {useEffect, useState} from 'react';
 import Connector from '../../services/signalr-connection';
 import {AttendeeEntity} from "../../models/attendeeEntity";
 import "./welcome.css";
+import InnovateFont from "../../components/shared/innovateFont/innovateFont";
 
 function Welcome() {
   const {onAttendeeSelected} = Connector();
 
   const [attendee, setAttendee] = useState<AttendeeEntity>();
-
-  function debounce<F extends (...args: any[]) => any>(func: F, waitFor: number) {
-    let timeoutId: NodeJS.Timeout | null = null;
-
-    return (...args: Parameters<F>): void => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-
-      timeoutId = setTimeout(() => func(...args), waitFor);
-    };
-  }
+  const [showAttendee, setShowAttendee] = useState<boolean>(false);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
 
   useEffect(() => {
-    const handleAttendeeSelected = (attendee?: AttendeeEntity) => {
-      setAttendee(attendee);
 
-      debounce(() => {
-        setAttendee(undefined);
-      }, 30000)();
-    }
+      const delay = (task: () => void, ms: number) => {
 
-    onAttendeeSelected((attendee) => handleAttendeeSelected(attendee))
-  }, [onAttendeeSelected]);
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+        }
+
+        return new Promise<void>((resolve) => {
+          const id = setTimeout(() => {
+            task();
+            resolve();
+          }, ms);
+          setTimeoutId(id);
+        });
+      }
+
+      const clearAttendee = () => {
+
+        setShowAttendee(false);
+
+        return delay(() => {
+          setAttendee(undefined);
+        }, 1200);
+      }
+
+      const setAttendeeAndShow = (attendee?: AttendeeEntity) => {
+        setAttendee(attendee);
+        setShowAttendee(true);
+
+
+        delay(() => {
+          clearAttendee();
+        }, 6000);
+      }
+
+      const handleAttendeeSelected = async (newAttendee?: AttendeeEntity) => {
+        if (attendee) {
+
+          setShowAttendee(false);
+
+          return delay(() => {
+            setAttendeeAndShow(newAttendee);
+          }, 1200);
+        } else {
+          setAttendeeAndShow(newAttendee);
+        }
+      }
+
+      onAttendeeSelected((attendee) => handleAttendeeSelected(attendee))
+    }, [onAttendeeSelected, attendee, timeoutId]
+  )
 
   return (
     <div className="welcome">
-      {attendee && (
-        <>
-          <div className="attendee">
+      <div className={`attendee ${showAttendee && "show"}`}>
+        {attendee && (
+          <>
             {attendee.profilePictureUrl?.value &&
                 <img className="profile" src={attendee.profilePictureUrl.value} alt="Profile"/>}
-            <div>
-              <div className="name">{attendee.name?.value}</div>
+            <div className="details">
+              <InnovateFont className="name">{attendee.name?.value}</InnovateFont>
               <div className="title">{attendee.jobTitle?.value}</div>
             </div>
-          </div>
-          <div id="confettis">
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-            <div className="confetti"></div>
-          </div>
-        </>
-      )}
+            {/*<div id="confettis">*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*  <div className="confetti"></div>*/}
+            {/*</div>*/}
+          </>
+        )}
+      </div>
+      {/*<div id="decorations">*/}
+      {/*  <img src="/box.webp" alt="Box"/>*/}
+      {/*</div>*/}
     </div>
   );
 }
